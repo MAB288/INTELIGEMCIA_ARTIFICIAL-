11e39909849ce968ca13890182ea25f4d9bdb749
MAVIEL MARTINEZ CHOLULA <90359925+MAB288@users.noreply.github.com>
2025-02-11 23:17:37 -0600

    ejercicio 6

---
+++ ejercicio 6.py
@@ -0,0 +1,11 @@
+
+num1 = 7.0
+num2 = 7.0
+
+resta = num1 - num2
+multiplicacion = num1 * num2
+
+print(f"El resultado de la resta de {num1} - {num2} es: {resta}")
+print(f"El resultado de la multiplicación de {num1} * {num2} es: {multiplicacion}")
+
+print("¡Programa realizado por Maviel!")
