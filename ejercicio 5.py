772b0365fa5bfc3888f0f15b423d57e5b85f4d9a
MAVIEL MARTINEZ CHOLULA <90359925+MAB288@users.noreply.github.com>
2025-02-11 23:14:01 -0600

    ejercicio 5

---
+++ ejercicio 5.py
@@ -0,0 +1,11 @@
+num1 = float(input("numero1\n"))
+num2 = float(input("numero2\n"))
+resta = num1 - num2
+mult = num1 * num2
+
+print(f"\nla reta de{num1} y {num2} es : {resta}")
+
+print(f"\n la multiplicacion de {num1} y {num2} es: {mult}")
+
+
+print(f"\n!programa realizado por Maviel Martinez Cholula")
