394fb00bb96bdfcd9a1d0e047e0e0afeedfe04c0
MAVIEL MARTINEZ CHOLULA <90359925+MAB288@users.noreply.github.com>
2025-02-11 22:31:52 -0600

    ejercicio 3

---
+++ ejercicio 3.py
@@ -0,0 +1,10 @@
+nombre = "juan"
+edad= 25
+altura= 1.75
+
+print("nombre:", nombre)
+print("edad:", edad)
+print("altura:", altura )
+
+edad_en_5_años = edad +5
+print("en 5 años, tebre", edad_en_5_años, "años.")
